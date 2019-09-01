ea46afa16bda11ad17006cc7dae9f0624aa76ca3
Ganeshk750 <ganesh9ot@gmail.com>
2019-09-01 21:32:36 +0530

    Adding navbar routing home component and dashboard component in client

---
+++ client/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';  
+import { HomeComponent } from '../components/home/home.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+
+const appRoutes : Routes =[
+  {path: '',component: HomeComponent},
+  {path: 'dashboard',component: DashboardComponent},
+  {path: '**',component: HomeComponent}
+];
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  declarations: [],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
