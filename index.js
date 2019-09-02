592e20e470650a6afa3a8d5828bcb050074b7e36
Ganeshk750 <ganesh9ot@gmail.com>
2019-09-03 01:48:28 +0530

    adding validator of register route rest

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const path = require('path');
+const bodyParser = require('body-parser');
 const config = require('./config/database');
 
+const authentication = require('./routes/authentication')(router);
+
+///Database
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) =>{
     if(err){
@@ -13,8 +18,18 @@ mongoose.connect(config.uri, (err) =>{
     }
 });
 
+//parse application/x-www-from-urlencode
+app.use(bodyParser.urlencoded({extended: false}))
+//parse application/json
+app.use(bodyParser.json());
+
+////use for client build to access// nd  static dierctory for frontend
 app.use(express.static(__dirname + '/client/dist/client'));
 
+  //use for routing//
+  app.use('/authentication', authentication);
+
+// connect server to angular Index.html
 app.get('*', (req,res) =>{
     //res.send('<h1>Hello World</h1>');
     res.sendFile(path.join(__dirname + '/client/dist/index.html'));
---
+++ models/user.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+
+const bcrypt = require('bcrypt-nodejs');
+
+//here we are creating validater
+let emailLengthChecker = (email) => {
+    if(!email) return false;
+    else if(email.length < 5 || email.length > 30) return false;
+    else return true;
+  };
+  let validEmailChecker = (email) => {
+    if(!email) return false;
+    else {
+      const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+      return regExp.test(email);
+    }
+  };
+  
+  const emailValidators = [
+    {
+      validator: emailLengthChecker,
+      message: 'E-mail must be at least 5 characters but not more than 30'
+    },
+    {
+      validator: validEmailChecker,
+      message: 'must provide a valid email'
+    }
+  ]
+  
+  let usernameLengthChecker = (username) => {
+    if(!username) return false;
+    else if(username.length < 3 || username.length > 15) return false;
+    else return true;
+  };
+  let validUsername = (username) => {
+    if(!username) return false;
+    else {
+      let regExp = new RegExp(/^[a-zA-Z0-9]+$/)
+      return regExp.test(username)
+    }
+  };
+  
+  const usernameValidators = [
+    {
+      validator: usernameLengthChecker,
+      message: 'username must be at least 3 characters and not more than 15'
+    },
+    {
+      validator: validUsername,
+      message: 'username must not have any special character'
+    }
+  ]
+  
+  let passwordLengthChecker = (password) => {
+    if(!password) return false;
+    else if(password.length < 8 || password.length > 35) return false;
+    else return true;
+  };
+  let validPassword = (password) => {
+    if(!password) return false;
+    else {
+      let regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+      return regExp.test(password);
+    }
+  }
+  
+  const passwordValidators = [
+    {
+      validator: passwordLengthChecker,
+      message: 'password must be at least 8 characters long and no more than 35'
+    },
+    {
+      validator: validPassword,
+      message: 'password must have at least one uppercase, lowercase, number, and special character'
+    }
+  ]
+  
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
+  username: { type: String, required: true, unique: true, lowercase: true , validate: usernameValidators},
+  password: { type: String, required: true, validate: passwordValidators }
+});
+
+//creating middleware for encrypt the user password
+userSchema.pre('save',function(next) {
+    if(!this.isModified('password'))
+    return next();
+   
+
+    // here we are encrypt the password
+    bcrypt.hash(this.password, null, null, (err, hash) =>{
+        if(err) return next(err);
+        this.password = hash;
+        next();
+    });
+});
+
+
+// here we are bcrypt the password
+userSchema.methods.comparePassword = (password) =>{
+    return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', userSchema);
